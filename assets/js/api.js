c9694d3134cb74f494cd057d0debed8eaa9544ee
xcibe95x <xcibe95x@gmail.com>
2022-06-08 17:37:42 +0200

    Setup API cards

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,4 +1,20 @@
 
 popularAPI = "https://api.themoviedb.org/3/movie/popular?api_key=" + api_key;
+posterAPI = "https://image.tmdb.org/t/p/original";
 
-console.log(fetch(popularAPI));
+let posterContainer = document.querySelector(".poster-container");
+
+    fetch(popularAPI)
+    .then(result => result.json())
+    .then(film =>
+        film.results.forEach((e) => createCard(e.backdrop_path))
+    );
+
+function createCard(filmPoster) {
+    filmPoster = posterAPI + filmPoster;
+    posterContainer.innerHTML += `
+    <div class="movie-poster">
+        <span class="top-ten"></span>
+        <img src="${filmPoster}" alt="New Amsterdam">
+    </div>`
+}
