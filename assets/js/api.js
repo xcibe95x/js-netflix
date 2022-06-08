f928a3e6e1cadeb48a1ba2dd062e547173e433a5
xcibe95x <xcibe95x@gmail.com>
2022-06-08 20:08:15 +0200

    Remove html blocks and update api js

--- assets/js/api.js
+++ assets/js/api.js
@@ -2,19 +2,28 @@
 popularAPI = "https://api.themoviedb.org/3/movie/popular?api_key=" + api_key;
 posterAPI = "https://image.tmdb.org/t/p/original";
 
-let posterContainer = document.querySelector(".poster-container");
+let posterContainer = document.querySelectorAll(".poster-container");
 
     fetch(popularAPI)
     .then(result => result.json())
     .then(film =>
-        film.results.forEach((e) => createCard(e.backdrop_path))
+        film.results.forEach((e) => {
+            createCard(
+                e.backdrop_path,
+                e.title
+                )
+        })
     );
 
-function createCard(filmPoster) {
+function createCard(filmPoster, title) {
     filmPoster = posterAPI + filmPoster;
-    posterContainer.innerHTML += `
+    
+    for (i = 0; i <= posterContainer.length; i++) {
+    posterContainer[i].innerHTML += `
     <div class="movie-poster">
         <span class="top-ten"></span>
-        <img src="${filmPoster}" alt="New Amsterdam">
+        <span class="movie-title">${title}</span>
+        <img src="${filmPoster}" alt="${title}">
     </div>`
+    }
 }
