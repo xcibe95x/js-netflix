f0be276b1a1472f525d4ac82e97bbef4c47deec9
LeonyMalasanManalili <erlyon15@gmail.com>
2022-06-09 16:17:16 +0200

    sliderbutton

---
+++ assets/js/slidersButton.js
@@ -0,0 +1,12 @@
+let movieposters = document.querySelectorAll(".movie-section");
+
+for(let i = 0;i <= movieposters.length;i++)
+{ 
+    var dx = document.createElement("button")
+    var sx = document.createElement("button")
+    var dxArrow = createElement("i")
+    var sxArrow = createElement("i")
+}
+
+dx.appendChild(dxArrow);
+sx.appendChild(sxArrow);
