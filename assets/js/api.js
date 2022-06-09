2d4a680aa9b44b97832df663aaaf7e069c682958
LeonyMalasanManalili <erlyon15@gmail.com>
2022-06-09 21:22:28 +0200

    Revert "fetching?"
    This reverts commit e5b23c7a2bb86fcf3f6932428a3c60299f2c5896.

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,103 +1,70 @@
+
 // API Paths
-const baseURL = "https://api.themoviedb.org";
+const baseURL = "https://api.themoviedb.org"
 const popularAPI = baseURL + "/3/movie/popular?api_key=" + api_key + "&page=";
-const latestAPI = baseURL + "/3/movie/upcoming?api_key=" + api_key + "&language=en-US&page=";
-const topRatedAPI = baseURL + "/3/movie/top_rated?api_key=" + api_key + "&language=en-US&page=";
+const latestAPI = baseURL + "/3/movie/latest?api_key=" + api_key + "&language=en-US"
 const posterAPI = "https://image.tmdb.org/t/p/original";
-let paintedMovies = 0;
 
 // Get all query of the selected element
 function getContainer(string) {
-  return document.querySelectorAll(string);
+   return document.querySelectorAll(string);
 }
+
+// const posterContainer = document.querySelectorAll(".poster-container");
 
 // Call API for the number of Containers available
-for (i = 0; i < getContainer(".poster-container").length; i++) {
-  callAPI(i + 1, getContainer(".poster-container")[i], popularAPI);
+for (i = 0; i  < getContainer(".poster-container").length; i++) {
+    callAPI(i+1, getContainer(".poster-container")[i]);
 }
 
-// Populate Big Movie Posters
-callAPI(1, getContainer(".big-poster-container")[0], latestAPI, 1);
-
-// Populate TOP 10
-callAPI(1, getContainer(".top-poster-container")[0], topRatedAPI, 2);
-
-// Fetch API data for and print the card
-function callAPI(page, container, api, cardType) {
-  fetch(api + page)
-    .then((result) => result.json())
-    .then((film) => {
-      film.results.forEach((e, i) => {
-        createCard(container, e.backdrop_path, e.title, e.id, e.poster_path, cardType, i + 1, e.vote_average);
-      });
-    });
-}
-
-// Check if the Movie is High Rated
-function Rating(factor) {
-  if (factor >= 7.8) {
-    return `<span class="top-ten"></span>`;
-  } else {
-    return ``;
-  }
+// Fetch API data for and print the card 
+function callAPI (page, container) {
+    fetch(popularAPI + page)
+    .then(result => result.json())
+    .then(film =>
+        film.results.forEach((e) => {
+            createCard (
+                container,
+                e.backdrop_path,
+                e.title,
+                e.id
+                )
+        })
+    )
 }
 
 // Create Film Cards
-function createCard(container, filmPoster, title, id, posterPath, cardType = 0, index, rating) {
-  let logoImage = "";
-  let logoAPI = baseURL + "/3/movie/" + id + "/images?api_key=" + api_key;
+function createCard(container, filmPoster, title, id, cardType = 0) {
+    let logoImage = ""; 
+    let logoAPI = baseURL + "/3/movie/" + id + "/images?api_key=" + api_key;
 
-  fetch(logoAPI)
-    .then((result) => result.json())
-    .then((logo) => {
-      // Make Logo File Path
-      if (logo.logos[0] != undefined) {
+    fetch(logoAPI)
+    .then(result => result.json())
+    .then(logo => {
+    
+    // Make Logo File Path
+    if (logo.logos[0] != undefined) {
+        console.log(logo.logos)
         logoImage = posterAPI + logo.logos[0].file_path;
-      }
+    }
 
-      // Suppress alternative text for empty array
-      let altTitle = title;
-      if (logo.logos.length == 0) {
-        altTitle = "";
-      }
+    // Make Image File Path
+    filmPoster = posterAPI + filmPoster;
 
-      // Regular Film Card
-      if (cardType == 0) {
-        // Add Code Snippet - Skip broken Film
-        if (id != 831728) {
-          container.innerHTML +=
-            `
-            <div id=${id} class="movie-poster">` +
-            Rating(rating) +
-            `
-                <img class="movie-logo" src="${logoImage}" alt="${altTitle}">
-                <img src="${posterAPI + filmPoster}" alt="${title}">
-            </div>`;
-        }
-      }
+    // Suppress alternative text for empty array
+    let altTitle = title;
+    if (logo.logos.length == 0) {     
+        altTitle = '';
+    }
 
-      // Big Film Card
-      if (cardType == 1) {
-        // Add Code Snippet - Skip broken Film
+    // Add Code Snippet - Skip broken Film
+    if (id != 831728) {
         container.innerHTML += `
-            <div id=${id} class="big-movie-poster">
-            <img src="${posterAPI + posterPath}" width="300px" alt="New Amsterdam" />
-            </div>`;
-      }
+        <div class="movie-poster">
+            <img class="movie-logo" src="${logoImage}" alt="${altTitle}">
+            <img src="${filmPoster}" alt="${title}">
+        </div>`
+    }
 
-      // Big Film Card
-      if (cardType == 2 && index <= 10) {
-        // Add Code Snippet - Skip broken Film
-        container.innerHTML += `
-            <div id=${id} order="${index}" class="movie-poster top10flex">
-                <span class="numberstop">${index}</span>
-                <img src="${posterAPI + posterPath}" alt="movietopimg" />
-            </div>`;
-        Array.from(container.children)
-          .sort((a, b) => a.order - b.order)
-          .forEach((e) => container.appendChild(e));
-      }
-      paintedMovies++;
-      if (paintedMovies >= 140) paintPlayer();
-    }); // close Fetch
+}) // close Fetch
 }
--- assets/js/poster.js
+++ assets/js/poster.js
@@ -1,22 +1,24 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 function paintPlayer() {
   let movie_Poster = document.querySelectorAll(".movie-poster");
   let media_Player = document.querySelector(".media-player");
+=======
+let movie_Poster = document.querySelectorAll(".movie-poster");
+let media_Player = document.querySelector(".media-player");
+console.log(movie_Poster);
+>>>>>>> parent of e5b23c7 (fetching?)
 
-  for (let i = 0; i < movie_Poster.length; i++) {
-    movie_Poster[i].addEventListener("mouseenter", function position(e) {
-      let rect = movie_Poster[i].getBoundingClientRect();
-      media_Player.style.top = cumulativeOffset(movie_Poster[i]) + "px";
-      media_Player.style.left = rect.x + "px";
-      media_Player.style.display = "block";
-    });
-  }
-
-  media_Player.addEventListener("mouseleave", () => {
-    media_Player.style.display = "none";
+for (let i = 0; i < movie_Poster.length; i++) {
+  movie_Poster[i].addEventListener("mouseenter", function position(e) {
+    let rect = movie_Poster[i].getBoundingClientRect();
+    media_Player.style.top = cumulativeOffset(movie_Poster[i]) + "px";
+    media_Player.style.left = rect.x + "px";
+    media_Player.style.display = "block";
   });
 }
 
+<<<<<<< HEAD
 =======
 let movie_Poster = document.querySelectorAll(".movie-poster");
 let media_Player = document.querySelector(".media-player");
@@ -31,11 +33,16 @@ for (let i = 0; i < movie_Poster.length; i++) {
   });
 }
 
+=======
+>>>>>>> parent of e5b23c7 (fetching?)
 media_Player.addEventListener("mouseleave", () => {
   media_Player.style.display = "none";
 });
 
+<<<<<<< HEAD
 >>>>>>> f0be276b1a1472f525d4ac82e97bbef4c47deec9
+=======
+>>>>>>> parent of e5b23c7 (fetching?)
 function cumulativeOffset(element) {
   var top = 0;
   do {
