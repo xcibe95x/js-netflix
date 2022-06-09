4671aaec84c72cd3d34dc50f08eb73b6cb2e2f23
xcibe95x <xcibe95x@gmail.com>
2022-06-09 14:46:55 +0200

    Update API Functions

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,29 +1,64 @@
 
-popularAPI = "https://api.themoviedb.org/3/movie/popular?api_key=" + api_key;
-posterAPI = "https://image.tmdb.org/t/p/original";
+// API Paths
+const baseURL = "https://api.themoviedb.org"
+const popularAPI = baseURL + "/3/movie/popular?api_key=" + api_key + "&page=";
+const latestAPI = baseURL + "/3/movie/latest?api_key=" + api_key + "&language=en-US"
+const posterAPI = "https://image.tmdb.org/t/p/original";
 
-let posterContainer = document.querySelectorAll(".poster-container");
+// Get all query of the selected element
+function getContainer(string) {
+   return document.querySelectorAll(string);
+}
 
-    fetch(popularAPI)
+// const posterContainer = document.querySelectorAll(".poster-container");
+
+// Call API for the number of Containers available
+for (i = 0; i  < getContainer(".poster-container").length; i++) {
+    callAPI(i+1, getContainer(".poster-container")[i]);
+}
+
+// Fetch API data for and print the card 
+function callAPI (page, container) {
+    fetch(popularAPI + page)
     .then(result => result.json())
     .then(film =>
         film.results.forEach((e) => {
-            createCard(
+            createCard (
+                container,
                 e.backdrop_path,
-                e.title
+                e.title,
+                e.id
                 )
         })
-    );
+    )
+}
 
-function createCard(filmPoster, title) {
-    filmPoster = posterAPI + filmPoster;
+// Create Film Cards
+function createCard(container, filmPoster, title, id) {
+    let logoImage = ""; 
+    let logoAPI = baseURL + "/3/movie/" + id + "/images?api_key=" + api_key;
+
+    fetch(logoAPI)
+    .then(result => result.json())
+    .then(logo => {
     
-    for (i = 0; i <= posterContainer.length; i++) {
-    posterContainer[i].innerHTML += `
-    <div class="movie-poster">
-        <span class="top-ten"></span>
-        <span class="movie-title">${title}</span>
-        <img src="${filmPoster}" alt="${title}">
-    </div>`
+    // Make Logo File Path
+    if (logo.logos[0] != undefined) {
+        logoImage = posterAPI + logo.logos[0].file_path;
     }
+    
+    
+    // Make Image File Path
+    filmPoster = posterAPI + filmPoster;
+
+    // Add Code Snippet - Skip broken Film
+    if (id != 831728) {
+        container.innerHTML += `
+        <div class="movie-poster">
+            <img class="movie-logo" src="${logoImage}" alt="${title}">
+            <img src="${filmPoster}" alt="${title}">
+        </div>`
+    }
+
+}) // close Fetch
 }
