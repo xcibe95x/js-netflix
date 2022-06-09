e5b23c7a2bb86fcf3f6932428a3c60299f2c5896
LeonyMalasanManalili <erlyon15@gmail.com>
2022-06-09 21:01:16 +0200

    fetching?

--- assets/js/poster.js
+++ assets/js/poster.js
@@ -1,19 +1,20 @@
-let movie_Poster = document.querySelectorAll(".movie-poster");
-let media_Player = document.querySelector(".media-player");
-console.log(movie_Poster);
+function paintPlayer() {
+  let movie_Poster = document.querySelectorAll(".movie-poster");
+  let media_Player = document.querySelector(".media-player");
 
-for (let i = 0; i < movie_Poster.length; i++) {
-  movie_Poster[i].addEventListener("mouseenter", function position(e) {
-    let rect = movie_Poster[i].getBoundingClientRect();
-    media_Player.style.top = cumulativeOffset(movie_Poster[i]) + "px";
-    media_Player.style.left = rect.x + "px";
-    media_Player.style.display = "block";
+  for (let i = 0; i < movie_Poster.length; i++) {
+    movie_Poster[i].addEventListener("mouseenter", function position(e) {
+      let rect = movie_Poster[i].getBoundingClientRect();
+      media_Player.style.top = cumulativeOffset(movie_Poster[i]) + "px";
+      media_Player.style.left = rect.x + "px";
+      media_Player.style.display = "block";
+    });
+  }
+
+  media_Player.addEventListener("mouseleave", () => {
+    media_Player.style.display = "none";
   });
 }
-
-media_Player.addEventListener("mouseleave", () => {
-  media_Player.style.display = "none";
-});
 
 function cumulativeOffset(element) {
   var top = 0;
