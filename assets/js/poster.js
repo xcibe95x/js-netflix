37b4c2626cd81891e5571fb8a87c466d2995f675
LeonyMalasanManalili <erlyon15@gmail.com>
2022-06-09 21:23:14 +0200

    Revert "Merge branch 'xcibe95x:main' into main"
    This reverts commit 68ca29064f511dc59ce3df187b97ee50a0c5aa65, reversing
    changes made to f0be276b1a1472f525d4ac82e97bbef4c47deec9.

--- assets/js/poster.js
+++ assets/js/poster.js
@@ -1,5 +1,6 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 function paintPlayer() {
   let movie_Poster = document.querySelectorAll(".movie-poster");
   let media_Player = document.querySelector(".media-player");
@@ -8,6 +9,11 @@ let movie_Poster = document.querySelectorAll(".movie-poster");
 let media_Player = document.querySelector(".media-player");
 console.log(movie_Poster);
 >>>>>>> parent of e5b23c7 (fetching?)
+=======
+let movie_Poster = document.querySelectorAll(".movie-poster");
+let media_Player = document.querySelector(".media-player");
+console.log(movie_Poster);
+>>>>>>> parent of 68ca290 (Merge branch 'xcibe95x:main' into main)
 
 for (let i = 0; i < movie_Poster.length; i++) {
   movie_Poster[i].addEventListener("mouseenter", function position(e) {
@@ -18,6 +24,7 @@ for (let i = 0; i < movie_Poster.length; i++) {
   });
 }
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 =======
 let movie_Poster = document.querySelectorAll(".movie-poster");
@@ -35,14 +42,19 @@ for (let i = 0; i < movie_Poster.length; i++) {
 
 =======
 >>>>>>> parent of e5b23c7 (fetching?)
+=======
+>>>>>>> parent of 68ca290 (Merge branch 'xcibe95x:main' into main)
 media_Player.addEventListener("mouseleave", () => {
   media_Player.style.display = "none";
 });
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 >>>>>>> f0be276b1a1472f525d4ac82e97bbef4c47deec9
 =======
 >>>>>>> parent of e5b23c7 (fetching?)
+=======
+>>>>>>> parent of 68ca290 (Merge branch 'xcibe95x:main' into main)
 function cumulativeOffset(element) {
   var top = 0;
   do {
