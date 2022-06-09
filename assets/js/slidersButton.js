f7e6e291bfd8a9af05b51a9a8e81e3abc40b4cd3
LeonyMalasanManalili <erlyon15@gmail.com>
2022-06-09 21:09:54 +0200

    Merge branch 'main' into development

---
+++ assets/js/slidersButton.js
@@ -0,0 +1,14 @@
+let movieposters = document.querySelectorAll(".poster-container");
+var dx = document.createElement("button")
+var sx = document.createElement("button") 
+var dxArrow = createElement("i")
+var sxArrow = createElement("i")
+
+dx.appendChild(dxArrow);
+sx.appendChild(sxArrow);
+
+for(let i = 0;i<=movieposters.length;i++)
+{ 
+  console.log(movieposters)
+}
+
